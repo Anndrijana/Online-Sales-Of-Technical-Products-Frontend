6739657d33168f712f3933f2885a374706023460
Anndrijana <papicandrijanica98@gmail.com>
2021-06-22 15:41:47 +0200

    Admin home css done

--- src/components/AdministratorHome/AdministratorHome.tsx
+++ src/components/AdministratorHome/AdministratorHome.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container, Card } from 'react-bootstrap';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faListOl, faShoppingBag, faStore } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Redirect, Link } from 'react-router-dom';
 import api, { ApiResponse } from '../../api/api';
 import RoledNavbar from '../RoledNavbar/RoledNavbar';
+import './AdministratorHome.css';
+import Img from './login.jpg';
 
 interface AdministratorHomeState {
     isAdministratorLoggedIn: boolean;
@@ -56,18 +58,51 @@ class AdministratorHome extends React.Component {
             <Container>
                 <RoledNavbar role="administrator"></RoledNavbar>
 
-                <Card>
-                    <Card.Body>
-                        <Card.Title>
-                            <FontAwesomeIcon icon={ faHome } /> Administrator Home
-                        </Card.Title>
-                        <ul>
-                            <li><Link to="/admin/home/category/">Categories</Link></li>
-                            <li><Link to="/admin/home/product/">Products</Link></li>
-                            <li><Link to="/admin/home/order/">Orders</Link></li>
-                        </ul>
-                    </Card.Body>
+                <Card className="bg-dark text-white">
+                    
+                    <Card.Img className="opacity" src={Img} alt="Card image"/>
+                
+                        <Card.ImgOverlay>
+                        <div className="adminHome">
+                            Welcome to administrator home
+                        </div>
+        
+                        <Card className="card1" style={{ width: '18rem' }}>
+                            <Card.Body>
+                            <Card.Title>
+                            <FontAwesomeIcon icon={ faListOl } size="lg" color="#C62E65" /> Categories
+                            </Card.Title>
+                            <Card.Text className="text-categories">
+                             Add, edit and delete categories
+                             </Card.Text>
+                             <Link className="link-2" to="/admin/home/category/">Click</Link>
+                             </Card.Body>
+                        </Card>
+                        <Card className="card2" style={{ width: '18rem' }}>
+                            <Card.Body>
+                            <Card.Title>
+                            <FontAwesomeIcon icon={ faStore } size="lg" color="#C62E65"/> Products
+                            </Card.Title>
+                            <Card.Text className="text-products">
+                             Add, edit and delete products
+                             </Card.Text>
+                             <Link className="link-2" to="/admin/home/product/">Click</Link>
+                             </Card.Body>
+                        </Card>
+                        <Card className="card3" style={{ width: '18rem' }}>
+                            <Card.Body>
+                            <Card.Title>
+                            <FontAwesomeIcon icon={ faShoppingBag } size="lg" color="#C62E65"/> Orders
+                            </Card.Title>
+                            <Card.Text className="text-orders">
+                             Add, edit and delete orders
+                             </Card.Text>
+                             <Link className="link-2" to="/admin/home/order/">Click</Link>
+                             </Card.Body>
+                        </Card>
+                        </Card.ImgOverlay>
                 </Card>
+
             </Container>
         );
     }
